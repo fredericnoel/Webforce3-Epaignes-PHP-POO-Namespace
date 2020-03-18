99bfc33a5444ae4377f34bcc0d745cf69999dcff
Frédéric NOËL <contact@fredericnoel.com>
2020-03-18 11:40:19 +0100

    Appel classe Instance objet classe Michel

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 date_default_timezone_set('Europe/Paris');
 require_once './functions/classAutoLoader.php';
-spl_autoload_register('classAutoloader');
+//spl_autoload_register('classAutoloader');
+
+require __DIR__ . '/classes/Michel/TestNameSpace.php';
 
 
+$toto = new Michel\TestNameSpace();
+echo $toto->display();
