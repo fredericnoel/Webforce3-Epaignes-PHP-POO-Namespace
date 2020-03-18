511ddb8d276262ec11cfdc61cc99a39c3a3c3963
Frédéric NOËL <contact@fredericnoel.com>
2020-03-18 14:20:09 +0100

    Syntaxe écriture Trait (une classe)

--- exempleTrait.php
+++ exempleTrait.php
@@ -1,1 +1,10 @@
 <?php
+
+/* On devrait le même au même niveau que les classes
+ * ou les interfaces, les traits sont un pattern de classes
+ * Je le mets ici pour que vous compreniez
+ * */
+
+trait DemoTrait
+{
+}
