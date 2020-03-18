7e66d9572315e4516317591ed6a264816d7b7e09
Frédéric NOËL <contact@fredericnoel.com>
2020-03-18 11:05:52 +0100

    Test namespace

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+date_default_timezone_set('Europe/Paris');
+require_once './functions/classAutoLoader.php';
+spl_autoload_register('classAutoloader');
+
+namespace Coronavirus;
+
+function var_dump() {
+    return "Coucou, je suis confine";
+}
+
+echo var_dump();
+
+\var_dump('coucou');
