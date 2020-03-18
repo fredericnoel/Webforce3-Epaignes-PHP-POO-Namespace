04c959f17a55811dd794e94d26ec0c7c97f19808
Frédéric NOËL <contact@fredericnoel.com>
2020-03-18 11:26:48 +0100

    Ecriture de classe avec définition d'un namespace

--- index.php
+++ index.php
@@ -4,12 +4,4 @@ date_default_timezone_set('Europe/Paris');
 require_once './functions/classAutoLoader.php';
 spl_autoload_register('classAutoloader');
 
-namespace Coronavirus;
 
-function var_dump() {
-    return "Coucou, je suis confine";
-}
-
-echo var_dump();
-
-\var_dump('coucou');
